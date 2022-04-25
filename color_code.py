8346897a8f22ad4440123229e552356445d8e3fd
dahmani <sonia.dahmani@etu.univ-paris-diderot.fr>
2022-04-25 20:43:34 +0200

    finalisation du code d'analyse des couleurs (se r

--- color_code.py
+++ color_code.py
@@ -4,69 +4,84 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import cv2
 from collections import Counter
-impot os
+# A Counter is a container that tracks how many times equivalent values are added.
 from skimage.color import rgb2lab, deltaE_cie76
 
 
+# color identification part
 
-#color identification part
-
-#to have a string vector of colors, we'll have to 
-#convert RGB to hex
-def RGBtoHEX(color): 
+# to have a string vector of colors, we'll have to
+# convert RGB to hex
+# returns a string, {:02x} displays the hex value
+def RGBtoHEX(color):
     return "#{:02x}{:02x}{:02x}".format(int(color[0]), int(color[1]), int(color[2]))
 
 
-#lecture de l'image en RGB
-def get_img(chemin):
-    image= cv2.imread(chemin)
-    image= cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# lecture de l'image en RGB
+def get_image(chemin):
+    image = cv2.imread(chemin)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
-#méthode qui prend les couleurs 
+# methode qui prend les couleurs
+
+
 def get_colors(image, nb_couleurs, tmp):
-    #on réduit la taille de l'image pour réduire le nombre de pixels à traiter
-    #et réduire le temps de traitement
-    new_format= cv2.resize(image, (600, 400), interpolation= cv2.INTER_AREA)
-    new_format= new_format.reshape(new_format.shape[0]*new_format.shape[1], 3)
+    # on reduit la taille de l'image pour reduire le nombre de pixels a traiter
+    # et reduire le temps de traitement
+    new_format = cv2.resize(image, (600, 400), interpolation=cv2.INTER_AREA)
+    new_format = new_format.reshape(new_format.shape[0]*new_format.shape[1], 3)
 
-    #utilisation de l'algo de k-partition
-    clf= KMeans(nb_clusters= nb_couleurs)
-    labels= clf.fit_predict(new_format) #Compute cluster centers and predict cluster index for each sample.
+    # utilisation de l'algo de k-partition
+    clf = KMeans(n_clusters=nb_couleurs)
+    # Compute cluster centers and predict cluster index for each sample.
+    labels = clf.fit_predict(new_format)
 
-    compteur= Counter(labels)
+    compteur = Counter(labels)
 
-    couleur_principales= clf.cluster_centers_
-    #on peut les ordonner  ?
-    ordered_colors= [couleur_principales[i] for i in compteur.keys()]
-    hex_colors = [RGBtoHEX(ordered_colors[i] for i in compteur.keys())]
+    couleur_principales = clf.cluster_centers_
+    # on peut les ordonner  ?
+    ordered_colors = [couleur_principales[i] for i in compteur.keys()]
+    # print(ordered_colors)
+    hex_colors = [RGBtoHEX(ordered_colors[i]) for i in compteur.keys()]
     rgb_colors = [ordered_colors[i] for i in compteur.keys()]
-
-    #if(tmp): 
-        #mettre les valeurs de compteur.values dans la donnée de sortie 
-        #un vecteur ou une liste ?? 
-        #où on renvoie direct compteur ? 
+    if(tmp):
+        # mettre les valeurs de compteur.values dans la donnee de sortie
+        # un vecteur ou une liste ??
+        # ou on renvoie direct compteur ?
         #
-    return rgb_colors
+        plt.figure(figsize=(8, 6))
+        print(compteur.values())
+        print("compteur: ", compteur)
+        plt.pie(compteur.values(), labels=hex_colors, colors=hex_colors)
+    # construire un dictionnaire pour renvoyer les valeurs
+
+    return hex_colors
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
 
- #reading images using cv2
- image= cv2.imread('tableau1.jpg')
- print("shape: {}".format(image.shape))
- #exemple d'affichage: (2456, 4856, 3)
- #deux première valeurs représentent le pixel, et la dernière
- #représente la couleur, 3 pour 3 couleurs RGB
- plt.imshow(image)
+    # reading images using cv2
+    image = cv2.imread('djurdjura.jpeg')
+    print("The type of this input is {}".format(type(image)))
+    print("shape: {}".format(image.shape))
+    # exemple d'affichage: (2456, 4856, 3)
+    # deux premiere valeurs representent le pixel, et la derniere
+    # represente la couleur, 3 pour 3 couleurs RGB
+    # OpenCV lit les images sous le format BGR, donc il
+    # faut les convertir dans le format RGB
+    # sinon l'affichage des couleurs n'est pas correct.
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    plt.imshow(image)
 
+    # to resize the image when needed
+    colors_string = get_colors(get_image('djurdjura.jpeg'), 8, True)
+    #print(colors_string[i] for i in range(len(colors_string)))
+    # color identification part
+    # RGB to hex conversion
+    print(colors_string)
+    plt.show()
 
-#OpenCV lit les images sous le format BGR, donc il 
-#faut les convertir dans le format RGB 
-#sinon l'affichage des couleurs n'est pas correct. 
-image= cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-plt.imshow(image)
-
-#to resize the image when needed 
-#new_format = cv2.resize(image, (1200, 600))
-#plt.imshow(resized_image)
+    # ce programme doit retourner:
+    # les valeurs en RGB et HEX des couleurs identifiees
+    # le pourcentage associe a la presence de chaque couleur dans le tableau
